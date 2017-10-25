fb15ac561fce7f65795ca4d60ad2348954cfa38b
Josh Neta <joshneta.github@gmail.com>
2017-10-25 17:50:09 -0500

    some cleanup

--- Assembly/blinddoor_hooks.asm
+++ Assembly/blinddoor_hooks.asm
@@ -1,7 +1,11 @@
 ;================================================================================
 ; Blind door close
 ;--------------------------------------------------------------------------------
-org $028849 ;original code : JSL $078000 //Hook on player main when transition are over execute player code
+; 
+org $028849 ; Bank02.asm(1588) - original code : JSL $078000 //Hook on player main when transition are over execute player code
 JSL check_special_action ;using the variable 7E0CF3 if it not 00 then trap the player in that room 
 ;could be changed easily to support more than only 1 function
 ;--------------------------------------------------------------------------------
+
+org $078000
+Player_Main:
